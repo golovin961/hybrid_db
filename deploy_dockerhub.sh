e7a9a95703df0adff4b7e88c4b41beb7a1efe859
golovin961 <golovin961@gmail.com>
2020-04-15 20:58:55 +0300

    10 attempt in travis ci

--- deploy_dockerhub.sh
+++ deploy_dockerhub.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-echo "$DOCKER_PASSWORD" | docker login  -u "$DOCKER_USERNAME" docker.io --password-stdin
-
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t $TRAVIS_REPO_SLUG:$TAG .
 docker push $TRAVIS_REPO_SLUG
